dd9ac845b3464378be8768de7145bb301295257e
Mohamed TAIEB <taieb.md@gmail.com>
2014-05-23 14:38:20 +0200

    Add Recaptcha
    Add recaptcha
    And REST insert

--- route.php
+++ route.php
@@ -1,8 +1,18 @@
 <?php
+require_once ('utils/recaptchalib.php');
+require_once ("utils/rest_utils.php");
+
+$privatekey = "6LdIzOoSAAAAANtnJTzZb3lfJSQtf-O96YVfyvFd";
+$resp = recaptcha_check_answer($privatekey, $_SERVER["REMOTE_ADDR"], $_POST["recaptcha_challenge_field"], $_POST["recaptcha_response_field"]);
+
+if (!$resp -> is_valid) {
+	$response = new RestResponse(405, json_encode(array("errorMessage" => "reCAPTCHA invalid")));
+	RestUtils::sendResponse($response);
+	die();
+}
+
+
 $base_url = "/lequipe/";
-
-require_once("utils/rest_utils.php");
-
 function to_camel_case($str) {
 	$str[0] = strtoupper($str[0]);
 	$func = create_function('$c', 'return strtoupper($c[1]);');
@@ -13,7 +23,7 @@ $request = RestUtils::processRequest();
 $response = new RestResponse(404, array("errorMessage" => "Resource not found"));
 
 // Ignore everything before the base url
-$route = substr($request->request_uri, strpos($request->request_uri, $base_url) + strlen($base_url));
+$route = substr($request -> request_uri, strpos($request -> request_uri, $base_url) + strlen($base_url));
 $uri_chunks = explode("/", $route);
 
 $controller = "";
@@ -24,7 +34,7 @@ $i = 0;
 // The first chunk is the controller
 if ($uri_chunks[$i]) {
 	$controller_file_name = $uri_chunks[$i++];
-	
+
 	// Ignore any GET parameters (they can still be accessed through the $data object)
 	if (strrpos($controller_file_name, "?")) {
 		$controller_file_name = substr($controller_file_name, 0, strrpos($controller_file_name, "?"));
@@ -41,16 +51,15 @@ $controller_file_name = 'petition';
 
 // Create a new instance of the given controller and call the given method with the given parameters
 if (file_exists("controllers/" . $controller_file_name . "_controller.php")) {
-	require_once("controllers/" . $controller_file_name . "_controller.php");
+	require_once ("controllers/" . $controller_file_name . "_controller.php");
 	$controller_name = to_camel_case($controller_file_name) . "Controller";
 	$controller = new $controller_name;
-	
-	if (method_exists($controller, $request->getMethod())) {
-		$response = $controller->{$request->getMethod()}($params, $request->getData());
+
+	if (method_exists($controller, $request -> getMethod())) {
+		$response = $controller -> {$request->getMethod()}($params, $request -> getData());
 	} else {
 		$response = new RestResponse(405, json_encode(array("errorMessage" => "Method not found")));
 	}
-	RestUtils::sendResponse($response);
 }
 
 RestUtils::sendResponse($response);
